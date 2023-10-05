b602acc63f8abbf8a8ce033bf05d81650f1d0e8e
have01 <have010101@gmail.com>
2023-10-05 22:22:40 +0530

    bookinformation vercel error

--- src/pages/BooksDetail.js
+++ src/pages/BooksDetail.js
@@ -1,17 +1,15 @@
 import React from 'react'
 import { useParams } from 'react-router'
 import useFetchBooks from '../customhooks/useFetchBooks'
-import { Rating } from '@mui/material'
 import Loader from '../components/Loader'
-import Bookabout from '../components/BookAbout'
-
+import BookInfomation from '../components/BookInfomation'
 function BooksDetail() {
     const { id } = useParams()
     const { data, loading } = useFetchBooks(`https://example-data.draftbit.com/books/${id}`)
 
     return (
         <>
-            {loading ? <Loader /> : <Bookabout data={data} />}
+            {loading ? <Loader /> : <BookInfomation data={data} />}
 
         </>
     )
