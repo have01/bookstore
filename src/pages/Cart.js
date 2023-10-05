f792e9da9f9ff6db454265965c5c6e23b3239fc6
have01 <have010101@gmail.com>
2023-10-05 22:58:35 +0530

    no items in the cart issue

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import CartProducts from "../components/CartProducts"
+import { useSelector } from 'react-redux'
 const Cart = () => {
+    const items = useSelector((state) => state?.cart)
     return (
-        <CartProducts />
+        <>
+            {items.length > 0 ? <CartProducts /> : <div className='container h-screen w-full flex justify-center mt-4'><h1 className=' text-xl sm:text-4xl'>No products in the cart</h1></div>}
+        </>
     )
 }
 
