459bc2c6c093e83d54f7ca4150a1d81f0f2de346
have01 <have010101@gmail.com>
2023-10-05 22:51:45 +0530

    navbar bg color issue

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -20,7 +20,7 @@ function Navbar() {
     };
     return (
         <>
-            <nav class="bg-white border-gray-200 dark:bg-gray-900 shadow-lg">
+            <nav class="bg-white border-gray-200  shadow-lg">
                 <div class="w-full flex  mx-auto  bg-slate-700  justify-center items-center">
                     <h1 className='text-lg text-white'>Welcome to BookStore</h1>
                 </div>
@@ -58,7 +58,7 @@ function Navbar() {
                         </button>
                     </div>
                     <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-user">
-                        <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                        <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white">
                             {Links?.map((val, index) => {
                                 return (
                                     <li><NavLink
